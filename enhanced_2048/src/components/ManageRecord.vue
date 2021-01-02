ac47c0609b2784cb9bff6372e8d97f291c766616
fullmoon-sg <mbfoo7757@gmail.com>
2021-01-02 04:43:42 +0000

    able to display hall of fame and player record

--- enhanced_2048/src/components/ManageRecord.vue
+++ enhanced_2048/src/components/ManageRecord.vue
@@ -1,23 +1,73 @@
 <template>
 <div class="main">
     <div class="left">
-        <h1>Hello World</h1>
+    <div>
+    <h1>Hall of Fame (TOP 10)</h1>
+    </div>
+    <div>
+      <table class="table">
+        <thead class="table-header">
+          <tr>
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Points</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(hf, index) in hall_fame" v-bind:key="hf._id">
+            <td>{{ index + 1 }}</td>
+            <td>{{ hf.name }}</td>
+            <td>{{ hf.score }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
     </div>
     <div class="right">
-    <h1>Welcome</h1>
+      <div>
+    <h1>Player Record</h1>
+    </div>
+    <div>
+      <table class="table">
+        <thead class="table-header">
+          <tr>
+            <th>Name</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(pc, index) in player_record" v-bind:key="pc._id">
+            <td>{{ index + 1 }}</td>
+            <td>{{ pc.name }}</td>
+            <td>{{ pc.score }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
      </div>
-
 </div>
     
 </template>
 
 <script>
+import axios from 'axios';
 export default {
     data : function(){
         return{
-            
+            'hall_fame' : [],
+            'player_record' : []
         }
-    }
+    },
+    created: async function () {
+    let response_hf = await axios.get(
+      "https://3000-fa64be6f-4931-4818-98d6-1cd8524de106.ws-us03.gitpod.io/"
+    );
+    this.hall_fame = response_hf.data;
+    let response_pc = await axios.get(
+      "https://3000-fa64be6f-4931-4818-98d6-1cd8524de106.ws-us03.gitpod.io/add"
+    );
+    this.player_record = response_pc.data;
+  },
     
 }
 </script>
